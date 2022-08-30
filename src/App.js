d02fdf9a537a428c411ff41b7be1717e85bf1f4d
pinaplehits <pinaplehits@outlook.com>
2022-08-30 17:47:37 -0600

    primer mockup de la nueva pagina web

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Navbar from "./components/Navbar"
+import Example from "./components/Example"
+import Features from "./components/Features"
+import Contact from "./components/Contact"
+import Header from "./components/Header"
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar />
+      <Header />
+      <Features />
+      <Contact />
+    </>
+  )
 }
-
-export default App;
