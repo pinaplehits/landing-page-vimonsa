7d5891b91a495dd4e1e50c3770bdabc2afdf7493
pinaplehits <pinaplehits@outlook.com>
2022-09-21 17:25:30 -0600

    Se elimino la dependecia de smooth scroll

--- src/App.js
+++ src/App.js
@@ -5,14 +5,6 @@ import Header from "./components/Header"
 import Services from "./components/Services"
 import Footer from "./components/Footer"
 import About from "./components/About"
-import SmoothScroll from "smooth-scroll"
-
-export const scroll = new SmoothScroll('a[href*="#"]', {
-  speed: 1000,
-  speedAsDuration: true,
-  updateURL: false,
-  offset: 65
-})
 
 export default function App() {
   return (
