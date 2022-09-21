7153aad7b631f1bea09cb4f641c08452b695b642
pinaplehits <pinaplehits@outlook.com>
2022-09-21 12:29:49 -0600

    smooth scroll agregado

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from "react"
 import Navbar from "./components/Navbar"
 import Features from "./components/Features"
 import Contact from "./components/Contact"
@@ -6,6 +5,14 @@ import Header from "./components/Header"
 import Services from "./components/Services"
 import Footer from "./components/Footer"
 import About from "./components/About"
+import SmoothScroll from "smooth-scroll"
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+  updateURL: false,
+  offset: 65
+})
 
 export default function App() {
   return (
